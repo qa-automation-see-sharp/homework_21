b2d4cd3de8a17dba0e9d28940420666c8975cab8
Ksenia <okstrishch@gmail.com>
2024-11-03 15:59:54 +0200

    Buttons and textBox pages

--- Tests.NUnit.Playwright/Tests/TextBoxPageTests.cs
+++ Tests.NUnit.Playwright/Tests/TextBoxPageTests.cs
@@ -1,7 +1,40 @@
+using Microsoft.Playwright;
+using Test.Utils.Fixtures;
+using Test.Utils.PageObjects;
+using BrowserType = Test.Utils.Fixtures.BrowserType;
+
 namespace Tests.NUnit.Playwright.Tests;
 
-//TODO: cover with tests
 [TestFixture]
 public class TextBoxPageTests
 {
+    private readonly BrowserSetUpBuilder _browserSetUp = new();
+    private MainPage Page { get; set; }
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        Page = await _browserSetUp
+            .WithBrowser(BrowserType.Chromium)
+            .InHeadlessMode(false)
+            .WithChannel("chrome")
+            .WithSlowMo(100)
+            .WithTimeout(10000)
+            .WithArgs("--start-maximized")
+            .OpenNewPage<MainPage>();
+    }
+
+    [Test]
+    public async Task OpenTextBoxPage()
+    {
+        await Page.OpenAsync();
+        await Page.Elements.ClickAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _browserSetUp.Page!.CloseAsync();
+        await _browserSetUp.Context!.CloseAsync();
+    }
 }
