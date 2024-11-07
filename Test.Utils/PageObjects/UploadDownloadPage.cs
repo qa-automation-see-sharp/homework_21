01a16ba43310d08fe417361617b38978cd3843cc
Liudmyla <savinska.l.y@gmail.com>
2024-11-07 21:20:27 +0100

    Added UploadDownloadPageTests

--- Test.Utils/PageObjects/UploadDownloadPage.cs
+++ Test.Utils/PageObjects/UploadDownloadPage.cs
@@ -1,6 +1,51 @@
+using Microsoft.Playwright;
+
 namespace Test.Utils.PageObjects;
 
-public class UploadDownloadPage
-{
+public class UploadDownloadPage : IBasePage
+    {
+    public IPage? Page { get; set; }
+    public string Url { get; } = "https://demoqa.com/upload-download";
+    public string ExpectedTitle { get; } = "Upload and Download";
     
+    public ILocator Title => Page!.Locator("xpath=//h1[text()='Upload and Download']");
+    public ILocator DownloadButton => Page!.Locator("xpath=/html//a[@id='downloadButton']");
+    public ILocator ChoosefileButton => Page!.Locator("xpath=/html//input[@id='uploadFile']");
+    public ILocator FilePath => Page!.Locator("//p[@id='uploadedFilePath']");
+    
+    public async Task<UploadDownloadPage> Open()
+    {
+        await Page!.GotoAsync(Url);
+        return this;
+    }
+
+    public async Task<UploadDownloadPage> FileUpload()
+    {
+        await ChoosefileButton.ClickAsync();
+        
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Test.Utils", "TestFiles", "NewFile1.txt");
+        await Page!.SetInputFilesAsync("input[type='file']", filePath);
+
+        return this;
+    }
+    
+    public async Task<UploadDownloadPage> FileDownload()
+    {
+        var downloadTask = Page!.RunAndWaitForDownloadAsync(async () =>
+        {
+            await DownloadButton.ClickAsync();
+        });
+        
+        var download = await downloadTask;
+
+        var downloadPath = await download.PathAsync();
+        
+        return this;
+    }
+    public async Task<UploadDownloadPage> ClosePage()
+    {
+        await Page!.CloseAsync();
+        return this;
+    }
+
 }
--- Tests.NUnit.Playwright/Tests/UploadDownloadPageTests.cs
+++ Tests.NUnit.Playwright/Tests/UploadDownloadPageTests.cs
@@ -1,7 +1,58 @@
+using Test.Utils.Fixtures;
+using Test.Utils.PageObjects;
+
 namespace Tests.NUnit.Playwright.Tests;
 
 //TODO: cover with tests
 [TestFixture]
 public class UploadDownloadPageTests
 {
+    private readonly BrowserSetUpBuilder _browserSetUpBuilder = new();
+    private UploadDownloadPage Page { get; set; }
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        Page = await _browserSetUpBuilder
+            .WithBrowser(BrowserType.Chromium)
+            .WithChannel("chrome")
+            .InHeadlessMode(false)
+            .WithTimeout(10000)
+            .WithArgs("--start-maximized")
+            .OpenNewPage<UploadDownloadPage>();
+        
+        await Page.Open();
+    }
+    
+    [Test]
+    public async Task GoToUploadDownloadPage_TitleIsCorrect()
+    {
+        var title = await Page.Title.TextContentAsync();
+
+        Assert.That(title, Is.EqualTo("Upload and Download"));
+    }
+
+    [Test]
+    public async Task DownloadFile_DownloadIsCorrect()
+    {
+        var download = await Page.FileDownload();
+        
+        Assert.That(download, Is.Not.Null);
+    }
+
+    [Test]
+
+    public async Task UploadFile_UploadIsCorrect()
+    {
+        var upload = await Page.FileUpload();
+        
+        Assert.That(upload, Is.Not.Null);
+    }
+    
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _browserSetUpBuilder.Context!.CloseAsync();
+        await Page.ClosePage();
+    }
 }
