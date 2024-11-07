4bfc9e67819babeb4972e4c90c74e3386c9127f8
Liudmyla <savinska.l.y@gmail.com>
2024-11-07 21:51:23 +0100

    Added DynamicProperties PO

--- Test.Utils/PageObjects/DynamicPropertiesPage.cs
+++ Test.Utils/PageObjects/DynamicPropertiesPage.cs
@@ -1,6 +1,24 @@
+using Microsoft.Playwright;
+
 namespace Test.Utils.PageObjects;
 
-public class DynamicPropertiesPage
-{
+public class DynamicPropertiesPage : IBasePage
+    {
+    public IPage? Page { get; set; }
+    public string Url { get; } = "https://demoqa.com/text-box";
+    public string ExpectedTitle { get; } = "Text Box";
     
+    public ILocator Title => Page!.Locator("xpath=//h1[text()='Text Box']");
+    
+    public async Task<DynamicPropertiesPage> Open()
+    {
+        await Page!.GotoAsync(Url);
+        return this;
+    }
+    
+    public async Task<DynamicPropertiesPage> ClosePage()
+    {
+        await Page!.CloseAsync();
+        return this;
+    }
 }
