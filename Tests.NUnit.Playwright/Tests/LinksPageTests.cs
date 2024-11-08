0807ca40dd59c2fd8c64d121b49a2127ec504658
Liudmyla <savinska.l.y@gmail.com>
2024-11-08 17:47:38 +0100

    added some tests

--- Tests.NUnit.Playwright/Tests/LinksPageTests.cs
+++ Tests.NUnit.Playwright/Tests/LinksPageTests.cs
@@ -1,7 +1,34 @@
+using Test.Utils.Fixtures;
+using Test.Utils.PageObjects;
+
 namespace Tests.NUnit.Playwright.Tests;
 
 //TODO: cover with tests
 [TestFixture]
 public class LinksPageTests
 {
+    private readonly BrowserSetUpBuilder _browserSetUpBuilder = new();
+    private LinksPage Page { get; set; }
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        Page = await _browserSetUpBuilder
+            .WithBrowser(BrowserType.Chromium)
+            .WithChannel("chrome")
+            .InHeadlessMode(false)
+            .WithTimeout(10000)
+            .WithArgs("--start-maximized")
+            .OpenNewPage<LinksPage>();
+        
+        await Page.Open();
+    }
+    
+    [Test]
+    public async Task GoToLinksPage_TitleIsCorrect()
+    {
+        var title = await Page.Title.TextContentAsync();
+
+        Assert.That(title, Is.EqualTo("Links"));
+    }
 }
