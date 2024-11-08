694311d5d20fa38c81cc70c07c3249bacc3b0a3e
Liudmyla <savinska.l.y@gmail.com>
2024-11-08 15:28:12 +0100

    Added locators

--- Test.Utils/PageObjects/DynamicPropertiesPage.cs
+++ Test.Utils/PageObjects/DynamicPropertiesPage.cs
@@ -9,6 +9,9 @@ public class DynamicPropertiesPage : IBasePage
     public string ExpectedTitle { get; } = "Dynamic Properties";
     
     public ILocator Title => Page!.Locator("xpath=//h1[text()='Dynamic Properties']");
+    public ILocator EnableAfterButton => Page!.Locator("xpath=/html//button[@id='enableAfter']");
+    public ILocator ColorChangeButton => Page!.Locator("xpath=/html//button[@id='colorChange']");
+    public ILocator VisibleAfterButton => Page!.Locator("xpath=/html//button[@id='visibleAfter']");
     
     public async Task<DynamicPropertiesPage> Open()
     {
