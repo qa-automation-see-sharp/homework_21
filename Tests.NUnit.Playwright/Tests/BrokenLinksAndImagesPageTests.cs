6ea5d17884045caf8234a50aa905e849db438350
Liudmyla <savinska.l.y@gmail.com>
2024-11-10 14:59:56 +0100

    Added BrokenLinks test

--- Tests.NUnit.Playwright/Tests/BrokenLinksAndImagesPageTests.cs
+++ Tests.NUnit.Playwright/Tests/BrokenLinksAndImagesPageTests.cs
@@ -1,7 +1,58 @@
+using Test.Utils.Fixtures;
+using Test.Utils.PageObjects;
+
 namespace Tests.NUnit.Playwright.Tests;
 
-//TODO: cover with tests
 [TestFixture]
 public class BrokenLinksAndImagesPageTests
 {
+    private readonly BrowserSetUpBuilder _browserSetUpBuilder = new();
+    private BrokenLinksAndImagesPage Page { get; set; }
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        Page = await _browserSetUpBuilder
+            .WithBrowser(BrowserType.Chromium)
+            .WithChannel("chrome")
+            .InHeadlessMode(false)
+            .WithTimeout(10000)
+            .WithArgs("--start-maximized")
+            .OpenNewPage<BrokenLinksAndImagesPage>();
+        
+        await Page.Open();
+    }
+    
+    [Test]
+    public async Task GoToButtonsPage_TitleIsCorrect()
+    {
+        var title = await Page.Title.TextContentAsync();
+
+        Assert.That(title, Is.EqualTo("Broken Links - Images"));
+    }
+    
+    [Test]
+    public async Task ClickOnValidLink_ReturnsCorrectTab()
+    {
+        await Page.ValidLink.ClickAsync();
+        var expectedPageUrl = Page.Url;
+
+        Assert.That(expectedPageUrl, Is.EqualTo(Page.Url));
+    }
+    
+    [Test]
+    public async Task ClickOnBrokenLink_ReturnsBrokenTab()
+    {
+        await Page.BrokenLink.ClickAsync();
+        await Task.Delay(3000);
+
+        Assert.That(Page.Page!.Url, Is.EqualTo(("http://the-internet.herokuapp.com/status_codes/500")));
+    }
+    
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _browserSetUpBuilder.Context!.CloseAsync();
+        await Page.ClosePage();
+    }
 }
