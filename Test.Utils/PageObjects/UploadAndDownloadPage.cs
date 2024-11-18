2448cc9703880ff06d3cd95b30219dd0edc53920
Sergii Zvonov <s.zvonov@gmail.com>
2024-11-18 22:07:06 +0200

    fix download test & implement links tests

--- Test.Utils/PageObjects/UploadAndDownloadPage.cs
+++ Test.Utils/PageObjects/UploadAndDownloadPage.cs
@@ -9,7 +9,7 @@ public class UploadAndDownloadPage : IBasePage
     public string ExpectedTitle { get; } = "Upload and Download";
 
     public ILocator Title => Page!.Locator("xpath=//h1[text()='Upload and Download']");
-    public ILocator DownloadButton => Page!.Locator("downloadButton");
+    public ILocator DownloadButton => Page!.Locator("#downloadButton");
 
     public async Task<UploadAndDownloadPage> Open()
     {
---
+++ Tests.NUnit.Playwright/Tests/LinksPageTests/LinksThatLeadToAnotherPage.cs
@@ -0,0 +1,59 @@
+using Test.Utils.Fixtures;
+using Test.Utils.PageObjects;
+
+namespace Tests.NUnit.Playwright.Tests.LinksPageTests;
+
+[TestFixture]
+public class LinksThatLeadToAnotherPage
+{
+    private readonly BrowserSetUpBuilder _browserSetUpBuilder = new();
+    private LinksPage Page { get; set; }
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUp()
+    {
+        Page = await _browserSetUpBuilder
+            .WithBrowser(BrowserType.Chromium)
+            .WithChannel("chrome")
+            .InHeadlessMode(true)
+            .WithTimeout(10000)
+            .WithSlowMo(100)
+            .WithArgs("--start-maximized")
+            .OpenNewPage<Test.Utils.PageObjects.LinksPage>();
+        _browserSetUpBuilder.AddRequestResponseLogger();
+        await Page.Open();
+    }
+
+    [Test]
+    public async Task OpenLinksPage()
+    {
+        var title = await Page.Title.TextContentAsync();
+
+        Assert.That(title, Is.EqualTo(Page.ExpectedTitle));
+    }
+
+    [Test]
+    public async Task ClickHomeLink_ReturnsCorrectTab()
+    {
+        await Page.HomeLink.ClickAsync();
+        var expectedUrl = Page.Page!.Url;
+
+        Assert.That(expectedUrl, Is.EqualTo(Page.Url));
+    }
+
+    [Test]
+    public async Task ClickHomeZDs2pLink_ReturnsCorrectTab()
+    {
+        await Page.HomeZDs2pLink.ClickAsync();
+        var expectedUrl = Page.Page!.Url;
+
+        Assert.That(expectedUrl, Is.EqualTo(Page.Url));
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
+        await _browserSetUpBuilder.Context!.CloseAsync();
+        await Page.ClosePage();
+    }
+}
