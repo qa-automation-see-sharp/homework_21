e48b81e70c2a1fd6f15316f721673c2c66ed02f5
Liudmyla <savinska.l.y@gmail.com>
2024-11-20 21:12:20 +0100

    Started adding RadioButtonTests

--- Tests.NUnit.Playwright/Tests/RadioButtonPageTests.cs
+++ Tests.NUnit.Playwright/Tests/RadioButtonPageTests.cs
@@ -1,7 +1,12 @@
+using Test.Utils.Fixtures;
+using Test.Utils.PageObjects;
+
 namespace Tests.NUnit.Playwright.Tests;
 
 //TODO: cover with tests
 [TestFixture]
 public class RadioButtonPageTests
 {
+    private readonly BrowserSetUpBuilder _browserSetUpBuilder = new();
+    private RadioButtonPage Page { get; set; }
 }
